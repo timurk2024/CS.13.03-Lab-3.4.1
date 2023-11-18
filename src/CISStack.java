00524ec6d64a8d0bd067b94b09dcdd086ecbcd47
Adam <adam.ewing.nz@gmail.com>
2023-11-18 12:44:37 +0800

    First commit.

--- src/CISStack.java
+++ src/CISStack.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CISStack {
+import java.util.LinkedList;
+
+public class CISStack {
+
+    // Linked list property.
+
+    // Size property.
+
+    // Constructor.
+
+    // Push. This method pushes a value onto the top of the stack.
+
+    // Pop. This method pops a value from the top of the stack.
+
+    // isEmpty. Returns a boolean indicating whether the linked list is empty.
+
+    // size. Returns the size of the queue.
+
+    // toString. Returns a description of the queue in, for example, the following format:
+    // CISStack{stack=[7, 11], size=2}
+
 }
